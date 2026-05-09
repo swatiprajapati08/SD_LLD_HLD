cb3ba65c1347e79b07351aadcec774fda04bb6fa
Swati Prajapati <prajapatiswati4@gmail.com>
2026-05-09 11:31:45 +0530

    adding command pattern

---
+++ src/LLD/DesignPattern/CommandPattern/Receiver/Fan.java
@@ -0,0 +1,11 @@
+package LLD.DesignPattern.CommandPattern.Receiver;
+
+public class Fan{
+    public String fanOn(){
+        return "FAN ON";
+    }
+
+    public String fanOff(){
+        return "FAN OFF";
+    }
+}
